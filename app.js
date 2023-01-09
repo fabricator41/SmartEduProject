3808de135f443224be8ae2f496b9fce8e4f74e20
Emre Ilhan <emrilhan@gmail.com>
2023-01-09 16:29:38 +0300

    Lesson 04

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 
 const app = express();
+
+// Connect DB
+
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+  console.log('DB connected succesfully');
+});
 
 // ! Template Engine
 
